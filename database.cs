359023ebaf129c556222a7cd880a053e7bb1c1c4
chencho11 <cboyle0731@gmail.com>
2021-12-08 18:47:07 -0500

    Update database.cs

--- database.cs
+++ database.cs
@@ -11,7 +11,7 @@ namespace ShopProject
         static string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Metro\\Documents\\GitHub\\ShopProject\\DBShop.mdf;Integrated Security=True;Connect Timeout=30";
         
 
-        public static bool Register(string Username, string Password, string Email)
+        public static bool Register(string Username, string Password)
         {
             SqlDataAdapter adapter = new SqlDataAdapter();
             SqlConnection conn = new SqlConnection(ConnectionString);
@@ -40,6 +40,26 @@ namespace ShopProject
             update.ExecuteNonQuery();
             conn.Close();
             return true; 
+        }
+        public static bool Login(string Username, string Password) //Can change return type if necessary
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand select = new SqlCommand("Select * from UserAccounts WHERE Username = @user AND Password = @pass");
+
+            select.Connection = conn;
+            select.Parameters.AddWithValue("@user", Username);
+            adapter.SelectCommand = select;
+            conn.Open();
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            conn.Close();
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
