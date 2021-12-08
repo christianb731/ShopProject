facde16acec37cd81769c22a265b0a93ec9253d0
chencho11 <cboyle0731@gmail.com>
2021-12-08 18:31:17 -0500

    Added database.cs
    Contains static methods for Registering and Logging in with database

---
+++ database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+namespace ShopProject
+{
+    public class DatabaseAccountAuthentication
+    {
+        static string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Metro\\Documents\\GitHub\\ShopProject\\DBShop.mdf;Integrated Security=True;Connect Timeout=30";
+        
+
+        public static bool Register(string Username, string Password, string Email)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand select = new SqlCommand("Select * from UserAccounts WHERE Username = @user");
+            
+            select.Connection = conn;
+            select.Parameters.AddWithValue("@user", Username);
+            adapter.SelectCommand = select;
+            conn.Open();
+
+            Random rand = new Random();
+            int newID = rand.Next(1, 10000);
+            SqlCommand update = new SqlCommand("Insert into UserAccounts (Id, Username, Password) VALUES (@id, @user, @pass)");
+            update.Parameters.AddWithValue("@id", newID);
+            update.Parameters.AddWithValue("@user", Username);
+            update.Parameters.AddWithValue("@pass", Password);
+            update.Connection = conn;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            if(dt.Rows.Count > 0)
+            {
+                conn.Close();
+                return false; //If registration fails here, it is because the username is already in use.
+            }
+            update.ExecuteNonQuery();
+            conn.Close();
+            return true; 
+        }
+    }
+}
