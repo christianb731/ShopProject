8d357c8f9c8c29f49267e7ca99a4df31b1b14a68
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2021-12-08 19:35:46 -0500

    Revert "About page"
    This reverts commit 12dbb9a3b12d5ffdbf2693b0722d12e7173c30c5.

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,14 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Configuration;
 
 namespace ShopProject
 {
     public partial class Login : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ASPNETDB"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,33 +17,6 @@ namespace ShopProject
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
 
-        }
-
-        protected void Button1_Click(System.Object sender, System.EventArgs e)
-        {
-            try
-            {
-                string uid = TextBox1.Text;
-                string pass = TextBox2.Text;
-                con.Open();
-                string qry = "SELECT * from Customer where username='" + uid + "' and password='" + pass + "'";
-                SqlCommand cmd = new SqlCommand(qry, con);
-                SqlDataReader sdr = cmd.ExecuteReader();
-                if (sdr.Read())
-                {
-                    Response.Redirect("items.aspx");
-                }
-                else
-                {
-                    Label1.Text = "Username & Password Is not correct Try again..!";
-                }
-                con.Close();
-
-            }
-            catch (Exception ex)
-            {
-                Response.Write(ex.Message);
-            }
         }
     }
 }
--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -24,30 +24,12 @@ namespace ShopProject
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// TextBox1 control.
+        /// Login1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
-        /// TextBox2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
