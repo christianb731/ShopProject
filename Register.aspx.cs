ec2f09b19f4ae161969b2e80e1fdadb55eee34a5
chencho11 <cboyle0731@gmail.com>
2021-12-08 19:56:56 -0500

    Added login and Register Auth and Register page

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -5,19 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using static ShopProject.DatabaseAccountAuthentication;
-
 namespace ShopProject
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e)
         {
-           e.Authenticated = Login(Login1.UserName,Login1.Password);
+           bool success = Register(CreateUserWizard1.UserName, CreateUserWizard1.Password);
         }
     }
 }
